4c987c3d2337f7f3df2538403c0fedf987ec5c86
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-03-07 12:15:33 +0300

    add MFEs

---
+++ mfe-2/webpack.config.js
@@ -0,0 +1,13 @@
+const { merge } = require('webpack-merge');
+const singleSpaDefaults = require('webpack-config-single-spa-react');
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: 'test',
+    projectName: 'mfe-2',
+    webpackConfigEnv,
+    argv,
+  });
+
+  return merge(defaultConfig);
+};
