67ba1d5365bc35917b40bf8a5ac5a50c4684608b
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-03-12 12:01:36 +0300

    test

--- mfe-1/src/root.component.js
+++ mfe-1/src/root.component.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Title } from 'styled-library';
 
-const Title = styled.h1`
+const Title1 = styled.h1`
   font-size: 1.5em;
   text-align: center;
   color: red;
@@ -15,7 +16,8 @@ const Wrapper = styled.section`
 export default function Root(props) {
   return (
     <Wrapper>
-      <Title>Hello World!</Title>
+      <Title1>Hello World!</Title1>
+      <Title>Test</Title>
     </Wrapper>
   );
 }
