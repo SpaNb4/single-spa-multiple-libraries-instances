f2e023959e203ed482015f88390f9a00d45901a9
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-03-14 13:47:23 +0300

    test

--- mfe-1/src/root.component.js
+++ mfe-1/src/root.component.js
@@ -1,5 +1,70 @@
-import React from "react";
+// MFE 1
+import React from 'react';
+import { QueryClient, QueryClientProvider, useQuery, useQueryClient, useMutation } from 'react-query';
 
-export default function Root(props) {
-  return <section>React version <b>{React.version}</b> is mounted!</section>;
+const queryClient = new QueryClient();
+
+const fetchData = async () => {
+  const response = await fetch('http://localhost:3000/posts/');
+  return response.json();
+};
+
+const postTodo = async (todo) => {
+  const response = await fetch('http://localhost:3000/posts/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(todo),
+  });
+  return response.json();
+};
+
+function Todos() {
+  const queryClient = useQueryClient();
+  const { data, isLoading, isError } = useQuery({ queryKey: ['todos'], queryFn: fetchData });
+
+  const mutation = useMutation({
+    mutationFn: postTodo,
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ['todos'] });
+    },
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error fetching data</div>;
+
+  return (
+    <div>
+      <ul>
+        {data?.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+
+      <button
+        onClick={() => {
+          // mutation.mutate({
+          //   id: Date.now(),
+          //   title: 'Do Laundry',
+          // });
+          queryClient.invalidateQueries({ queryKey: ['todos'] });
+        }}
+      >
+        Add Todo
+      </button>
+    </div>
+  );
+}
+
+export default function MFE1() {
+  return (
+    <QueryClientProvider client={queryClient} contextSharing={true}>
+      <div>
+        <h1>MFE 1</h1>
+        <Todos />
+      </div>
+    </QueryClientProvider>
+  );
 }
--- mfe-2/src/root.component.js
+++ mfe-2/src/root.component.js
@@ -1,5 +1,58 @@
-import React from "react";
+// MFE 2
+import React from 'react';
+import { QueryClient, QueryClientProvider, useQuery, useQueryClient, useMutation } from 'react-query';
 
-export default function Root(props) {
-  return <section>React version <b>{React.version}</b> is mounted!</section>;
+const queryClient = new QueryClient();
+
+const fetchData = async () => {
+  const response = await fetch('http://localhost:3000/comments/');
+  return response.json();
+};
+
+function Todos() {
+  const queryClient = useQueryClient();
+  const { data, isLoading, isError } = useQuery({ queryKey: ['todos'], queryFn: fetchData });
+
+  // const mutation = useMutation({
+  //   mutationFn: postTodo,
+  //   onSuccess: () => {
+  //     // Invalidate and refetch
+  //     queryClient.invalidateQueries({ queryKey: ['todos'] });
+  //   },
+  // });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error fetching data</div>;
+
+  return (
+    <div>
+      <ul>
+        {data?.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+
+      <button
+      // onClick={() => {
+      //   mutation.mutate({
+      //     id: Date.now(),
+      //     title: 'Do Laundry',
+      //   });
+      // }}
+      >
+        Add Todo
+      </button>
+    </div>
+  );
+}
+
+export default function MFE1() {
+  return (
+    <QueryClientProvider client={queryClient} contextSharing={true}>
+      <div>
+        <h1>MFE 1</h1>
+        <Todos />
+      </div>
+    </QueryClientProvider>
+  );
 }
