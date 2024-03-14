c5ae34206fa43baebe036c25a8945da7ca9c1a83
Dzmitry Yarmoshkin <spanb4@gmail.com>
2024-03-14 12:02:52 +0300

    123

--- mfe-1/src/root.component.js
+++ mfe-1/src/root.component.js
@@ -1,5 +1,33 @@
-import React from "react";
+// MFE 1 - Component
+import React from 'react';
+import { Provider } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { increment, decrement } from './store'; // Import actions
+import { store, persistor } from './store'; // Import Redux store
 
-export default function Root(props) {
-  return <section>React version <b>{React.version}</b> is mounted!</section>;
+function Counter() {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+    </div>
+  );
+}
+
+export default function MFE1() {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <div>
+          <h1>MFE 1</h1>
+          <Counter />
+        </div>
+      </PersistGate>
+    </Provider>
+  );
 }
---
+++ mfe-2/src/store.js
@@ -0,0 +1,37 @@
+// MFE 2 - Redux Store
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const initialState = {
+  count: 0,
+};
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.count++;
+    },
+    decrement(state) {
+      state.count--;
+    },
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+export default counterSlice.reducer;
+
+const persistConfig = {
+  key: 'localStorageKey',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, counterSlice.reducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
